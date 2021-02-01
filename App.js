61731696bf4e0e6f0d42d31fdbba1ec60e6071ee
mc-perry <jonathan@m-craft.com>
2021-02-01 16:45:56 +0900

    Installed remaining necessary packages for sample app, and verified functionality of custom app bar on an actual device

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {Home, Restaurant, OrderDelivery} from './screens';
+
+import {Restaurant, OrderDelivery} from './screens';
+import Tabs from './navigation/tabs';
 
 const Stack = createStackNavigator();
 
@@ -10,9 +12,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{headerShown: false}}
+        screenOptions={{
+          headerShown: false,
+        }}
         initialRouteName={'Home'}>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
